a6772c1d00ce6c5127835e39ea77fb925773ca83
AhaD <abuahad35188@gmail.com>
2023-10-09 14:29:47 +0600

    product delet api

--- index.js
+++ index.js
@@ -87,6 +87,12 @@ const verifyAdmin = async (req, res, next) => {
      const result = await productCollection.insertOne(newItem)
      res.send(result);
    })
+   app.delete('/product/:id', verifyJWT, verifyAdmin, async (req, res) => {
+     const id = req.params.id;
+     const query = { _id: new ObjectId(id) }
+     const result = await productCollection.deleteOne(query);
+     res.send(result);
+   })
 
 
 
